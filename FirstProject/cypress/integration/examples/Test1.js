09f5fba095c115bdf2a341fe700c0e72c2dea300
Paulo Varassin <pauloarthurvm@hotmail.com>
2022-02-21 16:36:13 -0300

    Running on browsers

---
+++ FirstProject/cypress/integration/examples/Test1.js
@@ -0,0 +1,18 @@
+describe('My First Test Suite', function() {
+
+    it('My First Test Case', function() {
+
+        cy.visit('http://automationpractice.com/index.php');
+
+
+    })
+
+    it('My Second Test Case', function() {
+
+
+        
+    })
+
+
+
+})
