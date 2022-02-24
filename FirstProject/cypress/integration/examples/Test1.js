9b8e9f45c34933a769e47c2245d4f7e007264958
Paulo Varassin <pauloarthurvm@hotmail.com>
2022-02-24 14:04:06 -0300

    Alias to reuse locator - Handling Async Promises

--- FirstProject/cypress/integration/examples/Test1.js
+++ FirstProject/cypress/integration/examples/Test1.js
@@ -12,9 +12,14 @@ describe('My First Test Suite', function() {
         cy.get(".product:visible").should("have.length", 4)
         cy.get('.products').find('.product').should('have.length', 4)
 
-        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+        // Set it to a variable, for future UI locator modifications
+        cy.get('.products').as('productsLocator')
+        cy.get('@productsLocator').find('.product').eq(2).contains('ADD TO CART').click().then(function(){
+            console.log("Not cypress log method - but resolved by promise")
+        })
         // Same as above -> cy.get(':nth-child(3) > .product-action > button').click()
 
+        console.log("Not cypress log method")
 
         cy.get('.products').find('.product').each(($el, index, $list) => {
             const textVeg = $el.find('h4.product-name').text()
@@ -23,6 +28,10 @@ describe('My First Test Suite', function() {
             }
         })
 
+        // Assert logo name
+        cy.get('.brand').should('have.text', 'GREENKART')
+
+        // Print logo
         cy.get('.brand').then(function(logoelement) {
             cy.log(logoelement.text())
         })
