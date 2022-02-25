8cec215083796fab5e9393d9e414dfa6bd5b1f70
Paulo Varassin <pauloarthurvm@hotmail.com>
2022-02-25 14:11:43 -0300

    Handling Invisible Objects and Radio Buttons

--- FirstProject/cypress/integration/examples/Test3.js
+++ FirstProject/cypress/integration/examples/Test3.js
@@ -24,6 +24,17 @@ describe('My Third Test Suite', function() {
         })
         cy.get('#autocomplete').should('have.value', 'India')
 
+        // Handle Invisible Objects
+        cy.get('#displayed-text').should('be.visible')
+        cy.get('#hide-textbox').click()
+        cy.get('#displayed-text').should('not.be.visible')
+        cy.get('#show-textbox').click()
+        cy.get('#displayed-text').should('be.visible')
+
+        // Radio Buttons
+        cy.get('[value="radio2"]').check().should('be.checked')
+        
+
     })
 
 })
