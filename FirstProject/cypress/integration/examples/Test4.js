9c62e79432eddb96a670b047353a6303dc12ed03
Paulo Varassin <pauloarthurvm@hotmail.com>
2022-02-26 09:35:29 -0300

    Alerts popup - Mocha comparison

---
+++ FirstProject/cypress/integration/examples/Test4.js
@@ -0,0 +1,26 @@
+/// <reference types="Cypress" />
+
+describe('My Third Test Suite', function() {
+
+    it('TC01', function() {
+
+        // cy.visit('http://automationpractice.com/index.php');
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
+        cy.get('#alertbtn').click()
+        cy.get('[value="Confirm"]').click()
+
+        // Alert with only OK option
+        cy.on('window:alert', (str) => {
+            // Mocha comparison
+            expect(str).to.equal('Hello , share this practice page and share your knowledge')
+        })
+
+        // Alert with 2 buttons options
+        cy.on('window:confirm', (str) => {
+            // Mocha comparison
+            expect(str).to.equal('Hello , Are you sure you want to confirm?')
+        })
+
+    })
+
+})
