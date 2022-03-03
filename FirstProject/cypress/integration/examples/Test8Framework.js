8c35521aded7ce1f49579252783fe1dba7123b0b
Paulo Varassin <pauloarthurvm@hotmail.com>
2022-03-03 11:45:24 -0300

    Validating attribute and behaviour

--- FirstProject/cypress/integration/examples/Test8Framework.js
+++ FirstProject/cypress/integration/examples/Test8Framework.js
@@ -15,6 +15,9 @@ describe('My Eighth Test Suite', function() {
         cy.get("input[name='name']:nth-child(2)").type(this.data.name)
         cy.get("select").select(this.data.gender)
         
+        cy.get(":nth-child(4) > .ng-untouched").should("have.value", this.data.name)
+        cy.get("input[name='name']:nth-child(2)").should("have.attr", "minlength", "2")
+        cy.get("#inlineRadio3").should("be.disabled")
     })
 
 })
