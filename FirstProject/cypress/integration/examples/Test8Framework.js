b234b374f9156bbf8e0bb38014ad3e4f01dd6cac
Paulo Varassin <pauloarthurvm@hotmail.com>
2022-03-03 18:31:39 -0300

    PageObject into test steps

--- FirstProject/cypress/integration/examples/Test8Framework.js
+++ FirstProject/cypress/integration/examples/Test8Framework.js
@@ -1,10 +1,10 @@
 /// <reference types="Cypress" />
 import HomePage from "../pageObjects/HomePage"
-import HomePage from "../pageObjects/HomePage"
+import ProductPage from "../pageObjects/ProductPage"
 
 describe('My Eighth Test Suite', function() {
 
-    before(function() {
+    beforeEach(function() {
         // runs once before all rests in the block
         cy.fixture("test8framework").then(function(data) {
             this.data = data
@@ -31,7 +31,26 @@ describe('My Eighth Test Suite', function() {
     })
 
     it("TC02 - Same but using pageObject", function() {
-        const HomePage = new HomePage()
+        const homePage = new HomePage()
+        const productPage = new ProductPage()
+        cy.visit('https://rahulshettyacademy.com/angularpractice/')
+        homePage.getEditBox().type(this.data.name)
+        homePage.getGender().select(this.data.gender)
+        
+        homePage.getTowWayDataBinding().should("have.value", this.data.name)
+        homePage.getEditBox().should("have.attr", "minlength", "2")
+        homePage.getEntrepreneaur().should("be.disabled")
+
+        // Works as debug
+        // cy.pause()
+
+        homePage.getShopTab().click()
+
+        this.data.productName.forEach(function(element) {
+            cy.selectProduct(element)
+        })
+
+        productPage.getCheckoutButton().click()
 
     })
 
