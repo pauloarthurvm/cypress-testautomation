afed7acb04e1e1fd986233498b78de33fbad09d6
Paulo Varassin <pauloarthurvm@hotmail.com>
2022-03-09 10:40:28 -0300

    Implement csv testing features

--- FirstProject/cypress/integration/examples/SessionTest.js
+++ FirstProject/cypress/integration/examples/SessionTest.js
@@ -1,6 +1,7 @@
 /// <reference types="Cypress" />
 
 const neatCsv = require("neat-csv")
+let productName
 
 describe('My Session Test Suite', function() {
 
@@ -13,6 +14,9 @@ describe('My Session Test Suite', function() {
                     window.localStorage.setItem("token", Cypress.env("token"))
                 }
             })
+        })
+        cy.get(".card-body b").eq(1).then(function(el) {
+            productName = el.text()
         })
         cy.get(".card-body button:last-of-type").eq(1).click()
         cy.get("[routerlink*='cart']").click()
@@ -30,6 +34,8 @@ describe('My Session Test Suite', function() {
         cy.readFile("path/to/file.csv").then(async function(text) {
             const csv = await neatCsv(text)
             console.log(csv)
+            const actualProductCsv = csv[0]["Product Name"]
+            expect(productName).to.equal(actualProductCsv)
         })
     })
 })
